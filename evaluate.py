f54b010549de0b46e5b50ce449ed7e0ed1e2df92
ShuaiyiHuang <huangshuaiyi2013@163.com>
2017-08-22 16:56:59 +0800

    read attention run,but slow

--- evaluate.py
+++ evaluate.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
-from train import DrawModule
+from train_att import DrawModule
 
-batch_size=8
+batch_size=3
 enc_size=256
 dec_size=256
 z_size=10
@@ -21,7 +21,10 @@ log_interval=150
 
 
 def imshow(input):
-    numpy_images=input.numpy()
+    if use_cuda:
+        numpy_images=input.cpu().numpy()
+    else:
+        numpy_images = input.numpy()
     ##what is std=[a,b,c] and mean=[e,f,g]??
     std=0.5
     mean=0.5
@@ -35,17 +38,23 @@ def imshow(input):
     return
 
 if use_cuda:
-    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, use_att=use_att, T=T).cuda()
+    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
+                       use_att=use_att, T=T,img_size=img_size,N=N).cuda()
 else:
     model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, use_att=use_att, T=T)
-model.load_state_dict(torch.load('../models/DRAW/DRAW9'))
-imgs=model.generate(4)
+model.load_state_dict(torch.load('../models/DRAW/DRAW5'))
+imgs=model.generate(batch_size)
 print type(imgs)
 print len(imgs[0]),len(imgs),type(imgs[0][0]),imgs[0][0].shape
 
+h_dec_test=Variable(torch.randn(batch_size,dec_size)).cuda()
+gx,gy,delta,gamma,var=model.attention_param(h_dec_test)
+print gx,gy,delta,gamma,var
+Fx,Fy=model.filterbank(gx,gy,delta,var)
+
 for i in range(T):
     img_last=imgs[i]
-    print type(img_last),img_last.shape
+    # print type(img_last),img_last.shape
 
     tensor_img_last=torch.FloatTensor(img_last)
     tensor_img_reshape=tensor_img_last.view(-1,1,28,28)
@@ -54,4 +63,19 @@ for i in range(T):
     plt.figure(i)
     imshow(img_grid)
 
-# images_grid=torchvision.utils.make_grid(images)
+train_transforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+train_dataset = torchvision.datasets.MNIST(root='../data', download=True, train=True, transform=train_transforms)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,
+                                               num_workers=4)
+
+train_iter=iter(train_loader)
+images,labels=train_iter.next()
+x=Variable(images).cuda() if use_cuda else Variable(images)
+print x.data.size()
+model.batch_size=batch_size
+mus,sigmas,logsigmas,x_recons=model.forward(x)
+# loss_test=loss_func(x,mus,sigmas,logsigmas,x_recons)
+x_recons_tensors=x_recons.data
+grid_recons=torchvision.utils.make_grid(x_recons_tensors)
+imshow(grid_recons)
--- train_att.py
+++ train_att.py
@@ -8,14 +8,14 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
 
-batch_size=100
+batch_size=5
 enc_size=256
 dec_size=256
 z_size=10
 N=5
 img_size=28
-T=1
-use_att=False
+T=5
+use_att=True
 use_cuda=True
 lrt=1e-3
 log_interval=150
@@ -49,7 +49,7 @@ images_grid=torchvision.utils.make_grid(images)
 
 
 class DrawModule(nn.Module):
-    def __init__(self,batch_size,enc_size,dec_size,z_size,use_att,T):
+    def __init__(self,batch_size,enc_size,dec_size,z_size,use_att,T,img_size,N):
         super(DrawModule, self).__init__()
         self.use_att=use_att
         self.batch_size=batch_size
@@ -57,11 +57,13 @@ class DrawModule(nn.Module):
         self.enc_size=enc_size
         self.dec_size=dec_size
         self.T=T
+        self.N=N
         self.z_size=z_size
         self.logsigmas, self.sigmas, self.mus= [0] * T, [0] * T, [0] * T
         self.mu_linear=nn.Linear(enc_size,z_size)
         self.sigma_linear=nn.Linear(enc_size,z_size)
         self.write_linear=nn.Linear(dec_size,img_size*img_size)
+        self.attparam_linear=nn.Linear(dec_size,5)
         #list.every element can be of different type
         self.Cs= [0] * T
         self.x_recons=None
@@ -124,9 +126,57 @@ class DrawModule(nn.Module):
 
     def read(self,x,x_hat,h_dec_prev):
         if self.use_att:
-            return None
+            x=x.view(-1,self.B,self.A)
+            x_hat=x_hat.view(-1,self.B,self.A)
+            gx,gy,gamma,delta,var=self.attention_param(h_dec_prev)
+            Fx,Fy=self.filterbank(gx,gy,delta,var)
+            # print Fy.size(),x.size(),x_hat.size()
+            a=torch.matmul(Fy,x)
+            Fy_t=torch.transpose(Fx,1,2)
+            x_att=torch.matmul(a,Fy_t)
+            x_att_hat=torch.matmul(torch.matmul(Fy,x_hat),torch.transpose(Fx,1,2))
+            x_att_view=x_att.view(-1,self.N*self.N)
+            x_att_hat_view=x_att_hat.view(-1,self.N*self.N)
+            x_read=gamma*torch.cat((x_att_view,x_att_hat_view),1)
+            # print x_read.size()
+            return x_read
         else:
             return torch.cat((x,x_hat),1)
+    def attention_param(self,h_dec):
+        att_param=self.attparam_linear(h_dec)
+
+        #Tensor:(Tensor) tensor to split.
+        #split_size:(int) size of a single chunk.
+        #dim:(int) dimension along which to split the tensor.
+        g_x_hat,g_y_hat,logvar,log_delta_hat,log_gamma=torch.split(att_param,1,1)
+        g_x=0.5*(self.A+1)*(g_x_hat+1)
+        g_y=0.5*(self.B+1)*(g_y_hat+1)
+        delta=torch.exp(log_delta_hat)*(np.max((self.A,self.B),0)-1)/(self.N-1)
+        gamma=torch.exp(log_gamma)
+        var=torch.exp(logvar)
+        return g_x,g_y,delta,gamma,var
+
+    def filterbank(self,gx,gy,delta,var):
+        if use_cuda:
+            Fx=Variable(torch.zeros(self.batch_size,self.N,self.A)).cuda()
+            Fy=Variable(torch.zeros(self.batch_size,self.N,self.B)).cuda()
+        for i in range(self.N):
+            mu_i_x = gx + (i - self.N * 0.5 - 0.5) * delta
+            for a in range(self.A):
+                Fx[:,i,a]=torch.exp(-(a-mu_i_x)*(a-mu_i_x)/(2*var))
+        for j in range(self.N):
+            mu_j_y=gy+(j-self.N*0.5-0.5)*delta
+            for b in range(self.B):
+                Fy[:,j,b]=torch.exp(-(b-mu_j_y)*(b-mu_j_y)/(2*var))
+        for batch_id in range(self.batch_size):
+            Zx=torch.sum(Fx[batch_id,:,:])
+            #Remember to use .clone,or you will encounter in-place error
+            Fx[batch_id,:,:]=Fx[batch_id,:,:].clone()/Zx
+        for batch_id in range(self.batch_size):
+            Zy=torch.sum(Fy[batch_id,:,:])
+            Fy[batch_id,:,:]=Fy[batch_id,:,:].clone()/Zy
+        # print Fx.size(),Fy.size()
+        return Fx,Fy
 
     def write(self,h_dec):
         return self.write_linear(h_dec)
@@ -167,9 +217,11 @@ class DrawModule(nn.Module):
         return imgs
 
 if use_cuda:
-    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, use_att=use_att, T=T).cuda()
+    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
+                       use_att=use_att, T=T,img_size=img_size,N=N).cuda()
 else:
-    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, use_att=use_att, T=T)
+    model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
+                       use_att=use_att, T=T,img_size=img_size,N=N)
 
 
 
@@ -224,6 +276,12 @@ def train(epoch):
     return
 
 if __name__=='__main__':
+    # h_dec_test = Variable(torch.randn(batch_size, dec_size)).cuda()
+    # gx, gy, delta, gamma, var = model.attention_param(h_dec_test)
+    # # print gx, gy, delta, gamma, var
+    # Fx, Fy = model.filterbank(gx, gy, delta, var)
+    # print torch.sum(Fx)
+    # print torch.sum(Fy)
     for i in range(10):
         train(i)
 
