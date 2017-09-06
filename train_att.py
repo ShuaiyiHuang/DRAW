b4ce3a622c2214f652b09dc15264c1f64620e518
ShuaiyiHuang <huangshuaiyi2013@163.com>
2017-09-06 09:41:40 +0800

    DRAW with att run but slow

--- train_att.py
+++ train_att.py
@@ -8,8 +8,9 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
 from utility import save_image,xrecons_grid
+import time
 
-batch_size=64
+batch_size=4
 enc_size=256
 dec_size=256
 z_size=10
@@ -17,11 +18,12 @@ N=5
 img_size=28
 A=img_size
 B=img_size
-T=5
+T=10
 use_att=True
 use_cuda=True
 lrt=1e-3
 log_interval=150
+epoch_num=20
 
 #you shouldn't use normalize if you use BCE loss and sigmoid for x_recons
 # train_transforms=transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
@@ -29,12 +31,17 @@ train_transforms=transforms.Compose([transforms.ToTensor()])
 train_dataset=torchvision.datasets.MNIST(root='../data',download=True,train=True,transform=train_transforms)
 train_loader=torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True,num_workers=4,drop_last=True)
 
+test_transforms=transforms.Compose([transforms.ToTensor()])
+test_dataset=torchvision.datasets.MNIST(root='../data',download=True,train=False,transform=test_transforms)
+test_loader=torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=True,num_workers=4,drop_last=True)
+
 #how many batch=num of images/batch_size
-print "train dataset length:",len(train_loader)
+print "train dataloader length:",len(train_loader)
+print 'test dataloader length:',len(test_loader)
 #with mean and std
 def imshow(input):
     numpy_images=input.numpy()
-    ##what is std=[a,b,c] and mean=[e,f,g]??
+    ##what if std=[a,b,c] and mean=[e,f,g]??
     std=0.5
     mean=0.5
     img=numpy_images*std+mean
@@ -68,7 +75,7 @@ class DrawModule(nn.Module):
         self.logsigmas, self.sigmas, self.mus= [0] * T, [0] * T, [0] * T
         self.mu_linear=nn.Linear(enc_size,z_size)
         self.sigma_linear=nn.Linear(enc_size,z_size)
-        self.write_linear=nn.Linear(dec_size,img_size*img_size)
+
         self.attparam_linear=nn.Linear(dec_size,5)
         #list.every element can be of different type
         self.Cs= [0] * T
@@ -82,11 +89,14 @@ class DrawModule(nn.Module):
         self.sigmoid=nn.Sigmoid()
 
         #if use_cuda,specify lstm.cuda()?
-        if use_att:
+        if self.use_att:
+            print 'use attention'
             self.encoder=nn.LSTMCell(2*N*N,enc_size).cuda() if use_cuda else nn.LSTMCell(2*N*N,enc_size)
+            self.write_linear=nn.Linear(dec_size,self.N*self.N)
         else:
+            print 'no use attention'
             self.encoder=nn.LSTMCell(2*self.A*self.B,enc_size).cuda() if use_cuda else nn.LSTMCell(2*self.A*self.B,enc_size)
-
+            self.write_linear = nn.Linear(dec_size, self.A * self.B)
 
         self.decoder=nn.LSTMCell(z_size,dec_size).cuda() if use_cuda else nn.LSTMCell(z_size,dec_size)
         return
@@ -127,6 +137,7 @@ class DrawModule(nn.Module):
             z, self.mus[i], self.logsigmas[i], self.sigmas[i]=self.sampleZ(h_enc_prev)
             h_dec_prev,dec_state=self.Decode(z,(h_dec_prev,dec_state))
             self.Cs[i]= c_prev + self.write(h_dec_prev)
+
         self.x_recons=self.sigmoid(self.Cs[-1]).view(-1,1,self.A,self.B)
         return self.mus,self.sigmas,self.logsigmas,self.x_recons
 
@@ -138,12 +149,15 @@ class DrawModule(nn.Module):
             Fx,Fy=self.filterbank(gx,gy,delta,var)
             # print Fy.size(),x.size(),x_hat.size()
             a=torch.matmul(Fy,x)
-            Fy_t=torch.transpose(Fx,1,2)
-            x_att=torch.matmul(a,Fy_t)
+            #!!!!!
+            Fx_t=torch.transpose(Fx,1,2)
+            x_att=torch.matmul(a,Fx_t)
             x_att_hat=torch.matmul(torch.matmul(Fy,x_hat),torch.transpose(Fx,1,2))
             x_att_view=x_att.view(-1,self.N*self.N)
             x_att_hat_view=x_att_hat.view(-1,self.N*self.N)
-            x_read=gamma*torch.cat((x_att_view,x_att_hat_view),1)
+            concat=torch.cat((x_att_view,x_att_hat_view),1)
+
+            x_read=gamma*concat
             # print x_read.size()
             return x_read
         else:
@@ -174,18 +188,41 @@ class DrawModule(nn.Module):
             mu_j_y=gy+(j-self.N*0.5-0.5)*delta
             for b in range(self.B):
                 Fy[:,j,b]=torch.exp(-(b-mu_j_y)*(b-mu_j_y)/(2*var))
-        for batch_id in range(self.batch_size):
-            Zx=torch.sum(Fx[batch_id,:,:])
-            #Remember to use .clone,or you will encounter in-place error
-            Fx[batch_id,:,:]=Fx[batch_id,:,:].clone()/Zx
-        for batch_id in range(self.batch_size):
-            Zy=torch.sum(Fy[batch_id,:,:])
-            Fy[batch_id,:,:]=Fy[batch_id,:,:].clone()/Zy
+        Zx=torch.unsqueeze(torch.unsqueeze(Fx.sum(2).sum(1),1),2).expand_as(Fx)
+        Fx=Fx/Zx
+        Zy=torch.unsqueeze(torch.unsqueeze(Fy.sum(2).sum(1),1),2).expand_as(Fy)
+        Fy=Fy/Zy
+
+        # for batch_id in range(self.batch_size):
+        #     Zx=torch.sum(Fx[batch_id,:,:])
+        #     #Remember to use .clone,or you will encounter in-place error
+        #     Fx[batch_id,:,:]=Fx[batch_id,:,:].clone()/Zx
+        # for batch_id in range(self.batch_size):
+        #     Zy=torch.sum(Fy[batch_id,:,:])
+        #     Fy[batch_id,:,:]=Fy[batch_id,:,:].clone()/Zy
         # print Fx.size(),Fy.size()
         return Fx,Fy
 
     def write(self,h_dec):
-        return self.write_linear(h_dec)
+        if self.use_att:
+            wt=self.write_linear(h_dec)
+            wt=wt.view(-1,self.N,self.N)
+            gx,gy,gamma,delta,var=self.attention_param(h_dec)
+            Fx,Fy=self.filterbank(gx,gy,delta,var)
+
+
+            Fy_t=torch.transpose(Fy,1,2)
+
+            a=torch.matmul(Fy_t,wt)
+
+            to_write=torch.matmul(a,Fx)
+            # print Fy_t.size(),wt.size(),Fx.size(),a.size(),to_write.size()
+            # print 'gamma size:',gamma.size()
+            to_write=(1/gamma)*to_write.view(-1,self.A*self.B)
+            to_write=to_write.view(-1,self.B,self.A)
+        else:
+            to_write = self.write_linear(h_dec)
+        return to_write
 
     def normalSample(self):
         return Variable(torch.randn(self.batch_size,self.z_size)).cuda() if use_cuda else Variable(torch.randn(self.batch_size,self.z_size))
@@ -199,6 +236,7 @@ class DrawModule(nn.Module):
         return z,mu,logsigma,sigma
 
     def generate(self,batch_size=64):
+        #make sure that input batch_szie equals batch_size of the model
         self.batch_size = batch_size
         if use_cuda:
             h_dec_prev = Variable(torch.zeros(self.batch_size, self.dec_size), volatile=True).cuda()
@@ -223,8 +261,12 @@ class DrawModule(nn.Module):
         return imgs
 
 if use_cuda:
+
     model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
                        use_att=use_att, T=T,img_size=img_size,N=N).cuda()
+    # model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
+    #                    use_att=use_att, T=T,img_size=img_size,N=N)
+    # model=torch.nn.DataParallel(model).cuda()
 else:
     model = DrawModule(batch_size=batch_size, enc_size=enc_size, dec_size=dec_size, z_size=z_size, \
                        use_att=use_att, T=T,img_size=img_size,N=N)
@@ -248,7 +290,8 @@ def loss_func(x,mus,sigmas,logsigmas,x_recons):
     # print 'shape of sum KL',KL.size()
     KL_ave=torch.mean(KL)
     BCE=BCEcriterion(x_recons,x)*A*B
-    return KL_ave+BCE
+    # print 'BCE:',BCE.cpu().data.numpy()
+    return KL_ave+BCE,KL_ave,BCE
 
 
 
@@ -286,7 +329,7 @@ def train(epoch):
             avg_loss = 0
 
     print('====> Epoch: {} Average Train loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+          epoch, train_loss / len(train_loader)))
     #Not a directory
     torch.save(model.state_dict(),'../models/DRAW/'+'DRAW'+str(epoch))
 
@@ -304,12 +347,27 @@ def save_example_image():
     plt.matshow(imgs, cmap=plt.cm.gray)
     plt.savefig('image/example.png')
 
-def Train():
+def test(epoch):
+    test_loss=0
+    num_batch=0
+    for data,_ in test_loader:
+        if use_cuda:
+            inputs = Variable(data).cuda()
+        else:
+            inputs = Variable(data)
+        mus, sigmas, logsigmas, x_recons = model.forward(inputs)
+        loss, KLloss, BCEloss = loss_func(inputs, mus, sigmas, logsigmas, x_recons)
+        test_loss+=loss.cpu().data.numpy()
+        num_batch+=1
+    print('Epoch-{};Test loss:{}'.format(epoch,test_loss/num_batch))
+
+def main():
     count = 0
     avg_loss = 0
-    for epoch in range(10):
+    avg_BCEloss=0
+    for epoch in range(epoch_num):
         train_loss=0
-
+        start=time.time()
         for batch_idx,(data,_) in enumerate(train_loader):
 
             model.zero_grad()
@@ -318,11 +376,13 @@ def Train():
             else:
                 inputs=Variable(data)
             mus, sigmas, logsigmas, x_recons=model.forward(inputs)
-            loss=loss_func(inputs,mus,sigmas,logsigmas,x_recons)
+            # print inputs.size(),mus.size(),sigmas.size(),logsigmas.size(),x_recons.size()
+            loss,KLloss,BCEloss=loss_func(inputs,mus,sigmas,logsigmas,x_recons)
             loss.backward()
             optimizer.step()
             train_loss+=loss.data[0]
             avg_loss+=loss.cpu().data.numpy()
+            avg_BCEloss+=BCEloss.cpu().data.numpy()
             count=count+1
 
             # if batch_idx % log_interval == 0:
@@ -331,15 +391,18 @@ def Train():
             #         100. * batch_idx / len(train_loader),
             #         loss.data[0] / len(data)))
             if count % 100 == 0:
-                print 'Epoch-{}; Count-{}; loss: {};'.format(epoch, count, avg_loss / 100)
+                print 'Epoch-{}; Count-{}; loss: {};BCE loss:{}'.format(epoch, count, avg_loss / 100,avg_BCEloss/100)
                 if count % 3000 == 0:
                     torch.save(model.state_dict(), 'save/weights_%d.tar' % (count))
                     generate_image(count)
                 avg_loss = 0
+                avg_BCEloss=0
+        end=time.time()
+        print('====> Epoch: {} Average Train loss: {:.4f};elapsed time:{:.2f}'.format(
+              epoch, train_loss / len(train_loader),end-start))
 
-        print('====> Epoch: {} Average Train loss: {:.4f}'.format(
-              epoch, train_loss / len(train_loader.dataset)))
-        generate_image(count)
+        # test(epoch)
+
         # torch.save(model.state_dict(),'../models/DRAW/'+'DRAW'+str(epoch))
     return
 
@@ -350,7 +413,7 @@ if __name__=='__main__':
     # Fx, Fy = model.filterbank(gx, gy, delta, var)
     # print torch.sum(Fx)
     # print torch.sum(Fy)
-    Train()
+    main()
 
 
 
